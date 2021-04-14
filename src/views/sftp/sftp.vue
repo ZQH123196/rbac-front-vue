569d2d3e180b2b4eb658d81515e98c7f628a70c4
zqh <zqh123196@gmail.com>
2021-04-14 11:40:13 +0800

    update

--- src/views/sftp/sftp.vue
+++ src/views/sftp/sftp.vue
@@ -1,56 +1,103 @@
 <template>
-  <div>
-    <el-tree ref="tree" :data="data4list" :props="defaultProps" @node-click="handleTreeNodeClick" />
-  </div>
+    <div>
+        <el-tree
+            ref="tree"
+            :data="data4list"
+            :props="defaultProps"
+            @node-click="handleTreeNodeClick"
+        />
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'SftpPage',
-  data() {
-    return {
-      data4list: [],
-      defaultProps: {
-        children: 'children',
-        label: 'label'
-      }
+import request from "../../utils/request"
+
+// 递归休整数据
+function Map2List(map) {
+    let list = [];
+    for (let [k, v] of Object.entries(map)) {
+        if (typeof v === "object") {
+            list.push({ label: k, type: "DIRECTORY", children: Map2List(v) });
+        } else list.push({ label: k, type: "REGULAR" });
     }
-  },
-  created() {
-    this.data4list = [
-      {
-        type: 'DIRECTORY',
-        label: 'dirname',
-        children: []
-      },
-      {
-        type: 'REGULAR',
-        label: 'filename'
-      },
-      {
-        type: 'REGULAR',
-        label: 'filename'
-      },
-      {
-        type: 'REGULAR',
-        label: 'filename'
-      }
-    ]
-  },
-  methods: {
-    handleTreeNodeClick(dataObj, nodeObj, treeObj) {
-      console.log('dataObj', dataObj)
-      console.log('nodeObj', nodeObj)
-      console.log('treeObj', treeObj)
-      // 如果是文件夹就展开，其他类型都下载
-      if (dataObj.type === 'DIRECTORY') {
-        // do nothing
-      } else {
-        // download xxx
-        console.log('download xxx')
-      }
-    }
-  }
+    return list;
 }
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export default {
+    name: "SftpPage",
+    data() {
+        return {
+            data4list: [],
+            defaultProps: {
+                children: "children",
+                label: "label",
+            },
+        };
+    },
+    created() {
+        let data = {
+            a: "a",
+            "通用核心题.md": "通用核心题.md",
+            dir1: {
+                dir2: {
+                    "IOC.md": "IOC.md",
+                    dir3: {
+                        "摘要算法.md": "摘要算法.md",
+                        "lifetime.md": "lifetime.md",
+                    },
+                },
+                dir1: {
+                    dir1: {},
+                },
+                "数字证书_原理.md": "数字证书_原理.md",
+                "编码算法.md": "编码算法.md",
+            },
+            "摘要算法.md": "摘要算法.md",
+            "管道通讯.md": "管道通讯.md",
+        };
+        this.data4list = [
+            {
+                type: "DIRECTORY",
+                label: "dirname",
+                children: [],
+            },
+            {
+                type: "REGULAR",
+                label: "filename",
+            },
+            {
+                type: "REGULAR",
+                label: "filename",
+            },
+            {
+                type: "REGULAR",
+                label: "filename",
+            },
+        ];
+
+        this.data4list = Map2List(data);
+        console.log("this.data4list", this.data4list);
+    },
+    methods: {
+        handleTreeNodeClick(dataObj, nodeObj, treeObj) {
+            console.log("dataObj", dataObj);
+            console.log("nodeObj", nodeObj);
+            console.log("treeObj", treeObj);
+            // 如果是文件夹就展开，其他类型都下载
+            if (dataObj.type === "DIRECTORY") {
+              // do nothing
+            } else {
+              // download xxx
+              request.get()
+
+              console.log("download xxx");
+            }
+        },
+    },
+};
 </script>
 
